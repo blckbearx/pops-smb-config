3e1d7c288f84789fd1f56eb2219b448b33eae787
Ignacio Grosso <nachogsr@gmail.com>
2025-11-19 18:14:41 -0300

    math: Remove old commented code.

--- math.h
+++ math.h
@@ -1,10 +1,5 @@
 /* math.h */
 
-// void subsTen(int *a, int *b, int *c, int *d, int *e, int x);
-// void plusTen(int *a, int *b, int *c, int *d, int *e, int x);
-// void subsOne(int *a, int *b, int *c, int *d, int *e, int x);
-// void plusOne(int *a, int *b, int *c, int *d, int *e, int x);
-
 void subsTen(int *ip_array, int index);
 void plusTen(int *ip_array, int index);
 void subsOne(int *ip_array, int index);
